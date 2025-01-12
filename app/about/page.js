9f1cf2f9708d7eb117fbe5022cc2d5551e654566
Ahmed <ahmed.khalaf.1088@gmail.com>
2025-01-12 02:39:41 +0100

    Adding metadata, favicon and fonts

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
 export default function page() {
   return <h2>About The Wild Oasis</h2>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,28 @@
 import Logo from "@/app/_componants/Logo";
 import Navigation from "@/app/_componants/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcom / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100">
+      <body className={`${josefin.className} bg-primary-950 text-primary-100`}>
         <header>
           <Logo />
           <Navigation />
