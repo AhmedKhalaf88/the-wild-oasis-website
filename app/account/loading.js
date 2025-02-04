842073e9ac88cf7faee1bf6a6082e068ffd533f4
Ahmed <ahmed.khalaf.1088@gmail.com>
2025-02-04 23:33:11 +0100

    Removing reservation immediately

---
+++ app/account/loading.js
@@ -0,0 +1,5 @@
+import Spinner from "@/app/_componants/Spinner";
+
+export default function Loading() {
+  return <Spinner />;
+}
--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,4 +1,4 @@
-import ReservationCard from "@/app/_componants/ReservationCard";
+import ReservationList from "@/app/_componants/ReservationList";
 import { auth } from "@/app/_lib/auth";
 import { getBookings } from "@/app/_lib/data-service";
 
@@ -24,11 +24,7 @@ export default async function Page() {
           </a>
         </p>
       ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
+        <ReservationList bookings={bookings} />
       )}
     </div>
   );
